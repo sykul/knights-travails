86aa92f09f72923c14a8c1c97ce01f5c6b0c4744
sykul <luke.flatley@outlook.com>
2024-10-31 12:25:52 +0100

    update

--- knight-functions.js
+++ knight-functions.js
@@ -16,11 +16,34 @@ function isValidPosition(arr) {
   };
 }
 
+
+function levelOrder(callback) {
+  if (!callback instanceof Function) {
+    throw new Error('Parameter is not a function!');
+  }
+
+  if (this.root === null) {
+    return;
+  }
+
+  let queue = [];
+  queue.push(this.root);
+  while(queue.length > 0) {
+    let currentNode = queue[0];
+    if (currentNode.leftChild !== null) {
+      queue.push(currentNode.leftChild);
+    }
+    if (currentNode.rightChild !== null) {
+      queue.push(currentNode.rightChild);
+    }
+    callback(queue.shift().data)
+  }
+}
+
 function knightMoves(arr1, arr2) {
   isValidPosition(arr1);
   isValidPosition(arr2);
-
-  const unvisited = [] 
+  
 
 }
 
